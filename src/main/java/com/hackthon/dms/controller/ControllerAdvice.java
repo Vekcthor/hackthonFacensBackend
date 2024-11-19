2e925f5f7d261910af25c8f9cdbf9c4eaefae454
Thirbt <thiago.godo@hotmail.com>
2024-11-19 14:10:36 -0300

    add: Adicionando um ControllerAdvice para tratamento de erros na controller

---
+++ src/main/java/com/hackthon/dms/controller/ControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.hackthon.dms.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
+
+import com.hackthon.dms.dto.exception.ErrorDTO;
+import com.hackthon.dms.exception.GeneralApiError;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(GeneralApiError.class)
+    public ErrorDTO handlerGeneralApiError(GeneralApiError error){
+        return new ErrorDTO(error.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorDTO handlerGenericApiError(Exception error){
+        return new ErrorDTO(error.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ErrorDTO handlerMultipartApiError(MissingServletRequestParameterException  error){
+        return new ErrorDTO("The current '" + error.getParameterName() + "' is missing");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MultipartException .class)
+    public ErrorDTO handlerMultiApiError(MultipartException   error){
+        return new ErrorDTO(error.getMessage());
+    }
+}
