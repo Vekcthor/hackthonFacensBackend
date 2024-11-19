52b723f16641dd89a38e3aa5808f7fc025e7052c
Thirbt <thiago.godo@hotmail.com>
2024-11-19 14:13:00 -0300

    fix: Adicionando validações de tratamento de erro dentro da service

--- src/main/java/com/hackthon/dms/service/FileService.java
+++ src/main/java/com/hackthon/dms/service/FileService.java
@@ -1,8 +1,10 @@
 package com.hackthon.dms.service;
 
+import com.hackthon.dms.exception.GeneralApiError;
 import com.hackthon.dms.model.EncryptedFile;
 import com.hackthon.dms.repository.FileRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -18,7 +20,7 @@ public class FileService {
     @Autowired
     private FileRepository fileRepository;
 
-     private static final String ALGORITHM = "AES";
+    private static final String ALGORITHM = "AES";
 
     public String generateKey() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
@@ -52,6 +54,37 @@ public class FileService {
 
     public EncryptedFile getFile(Long id) {
         return fileRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("File not found"));
+                .orElseThrow(() -> new GeneralApiError("File not found in the base"));
+    }
+
+    public String processUpload(MultipartFile file, String fileName) throws Exception {
+        if (file == null || file.isEmpty()) {
+            throw new GeneralApiError("File is required and cannot be empty.");
+        }
+        if (fileName == null || fileName.isBlank()) {
+            throw new GeneralApiError("File Name is required and cannot be empty.");
+        }
+        String key = generateKey();
+        byte[] encryptedContent = encrypt(file.getBytes(), key);
+        saveFile(encryptedContent, key, fileName);
+        return key;
+    }
+
+    public byte[] processDownload(Long id, String key) throws Exception {
+        EncryptedFile file = getFile(id);
+
+        if (!file.getEncryptionKey().equals(key)) {
+            throw new GeneralApiError("Invalid key");
+        }
+
+        if (file.getKeyExpiration().isBefore(LocalDateTime.now())) {
+            throw new GeneralApiError("Key has expired");
+        }
+
+        return decrypt(file.getEncryptedContent(), key);
+    }
+
+    public String getContentType(String fileName) {
+        return fileName.endsWith(".pdf") ? "application/pdf" : "application/octet-stream";
     }
 }
