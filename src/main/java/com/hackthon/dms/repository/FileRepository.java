704a074d5a1301e87b7a13b449e180c89f19cee9
Thirbt <thiago.godo@hotmail.com>
2024-11-21 13:22:44 -0300

    add: Adicionando um método para buscar o documento pelo seu valor random no repositório

--- src/main/java/com/hackthon/dms/repository/FileRepository.java
+++ src/main/java/com/hackthon/dms/repository/FileRepository.java
@@ -1,8 +1,12 @@
 package com.hackthon.dms.repository;
 
 import com.hackthon.dms.model.EncryptedFile;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<EncryptedFile, Long> {
+    Optional<EncryptedFile> findByRandomIdentification(Long randomIdentification);
 }
 
